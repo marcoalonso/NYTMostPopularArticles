ec6f459ba249105c6f756338c146e0ce8ab2e9de
Marco Alonso <marcoalonsorota@hotmail.com>
2024-11-28 14:41:47 -0600

    Initial Commit

---
+++ NewYorkTimesMostPopularArticles/NewYorkTimesMostPopularArticlesApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewYorkTimesMostPopularArticlesApp.swift
+//  NewYorkTimesMostPopularArticles
+//
+//  Created by Marco Alonso on 28/11/24.
+//
+
+import SwiftUI
+
+@main
+struct NewYorkTimesMostPopularArticlesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
